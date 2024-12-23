5b645075cec3bef7a71cc2894f2c87012124f2f3
ZGQ Inc. <zgqinc@gmail.com>
2024-12-23 14:47:04 +0800

    fix

--- _plugins/link.rb
+++ _plugins/link.rb
@@ -1,11 +1,11 @@
-module Jekyll
-    module AutoLinkFilter
-      def auto_link(input)
+# module Jekyll
+#     module AutoLinkFilter
+#       def auto_link(input)
 
-        input.gsub(/(https?:\/\/[\S]+)/, '<a href="\1" target="_blank">\1</a>')
-      end
-    end
-  end
+#         input.gsub(/(https?:\/\/[\S]+)/, '<a href="\1" target="_blank">\1</a>')
+#       end
+#     end
+#   end
   
-  Liquid::Template.register_filter(Jekyll::AutoLinkFilter)
+#   Liquid::Template.register_filter(Jekyll::AutoLinkFilter)
   
